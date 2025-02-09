fa82bbd89fca57c5ac70a84b9a1a5b39ac1c8100
Christopher Mahlangu <211159553@@tut4life.ac.za>
2025-02-09 19:39:50 +0200

    Refactor

--- src/main/kotlin/com/dochyphen/app/api/service/communication/AppNotificationService.kt
+++ src/main/kotlin/com/dochyphen/app/api/service/communication/AppNotificationService.kt
@@ -1,5 +1,8 @@
 package com.dochyphen.app.api.service.communication
 
+import jakarta.enterprise.context.ApplicationScoped
+
+@ApplicationScoped
 class AppNotificationService
 {
     fun sendNotification(appUserId: String, subject: String, body: String)
