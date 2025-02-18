e89e0f2e672e9c05ea92f14cc2f978717d35d4ec
Christopher Mahlangu <211159553@@tut4life.ac.za>
2025-02-18 23:17:22 +0200

    Upload dialog

---
+++ web-app/doc-hyphen/src/app/landing/components/session-document-add-dialog/DocumentAddDialogStyles.tsx
@@ -0,0 +1,24 @@
+import {makeStyles} from "@fluentui/react-components";
+
+export const useDocumentAddDialogStyles = makeStyles({
+    documentAddDialogContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '200px',
+        gap: "16px"
+    },
+
+    documentRestriction: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: '8px'
+    },
+    documentTitleField: {
+        width: '100%',
+        maxWidth: '400px',
+    },
+
+});
