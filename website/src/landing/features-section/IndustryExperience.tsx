bbcde5814f1d2dd2c980d9073ffbe592592245ad
Christopher Mahlangu <211159553@@tut4life.ac.za>
2026-08-09 23:48:01 +0200

    Update website text

--- website/src/landing/features-section/IndustryExperience.tsx
+++ website/src/landing/features-section/IndustryExperience.tsx
@@ -62,7 +62,7 @@ export function IndustryExperience({initialIndustrySlug}: IndustryExperienceProp
                     id="industry-product-experience-heading"
                     className={styles.heading}
                 >
-                    Documents are only the beginning..
+                    Documents are only the beginning.
                 </Title2>
                 <Text
                     id="industry-product-experience-description"
